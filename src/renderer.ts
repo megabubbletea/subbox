a8d8671bcd2889b6abc6ce1481509ee83d42ac7e
megabubbletea <growth_iambics0q@icloud.com>
2024-07-02 20:05:49 +0900

    Webpack 5 and Typescript

---
+++ src/renderer.ts
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let dropZone = document.getElementById('dropzone');
+
+    if (dropZone) {
+        // Prevent default drag behaviors
+        ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+            dropZone.addEventListener(eventName, preventDefaults, false);
+            document.body.addEventListener(eventName, preventDefaults, false);
+        });
+
+        // Highlight drop area when item is dragged over it
+        ['dragenter', 'dragover'].forEach(eventName => {
+            dropZone.addEventListener(eventName, highlight, false);
+        });
+
+        ['dragleave', 'drop'].forEach(eventName => {
+            dropZone.addEventListener(eventName, unhighlight, false);
+        });
+
+        // Handle dropped files
+        dropZone.addEventListener('drop', handleDrop, false);
+    }
+});
+
+function preventDefaults(e: Event) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+function highlight(e: Event) {
+    const dropZone = document.getElementById('dropzone');
+    if (dropZone) {
+        dropZone.classList.add('highlight');
+    }
+}
+
+function unhighlight(e: Event) {
+    const dropZone = document.getElementById('dropzone');
+    if (dropZone) {
+        dropZone.classList.remove('highlight');
+    }
+}
+
+function handleDrop(e: DragEvent) {
+    let dt = e.dataTransfer;
+    if (dt) {
+        let files = dt.files;
+        
+        // Process each file
+        // Array.from(files).forEach(processFile);
+    }
+}
+
+// function processFile(file: File) {
+//     if (file) {
+//         const reader = new FileReader();
+        
+//         reader.onload = function (e: ProgressEvent<FileReader>) {
+//             const arrayBuffer: ArrayBuffer | null = e.target?.result;
+//             let mp4boxfile: any;
+            
+//             if (arrayBuffer !== null) {
+//                 mp4boxfile = window.api.mp4box.createFile();
+//                 arrayBuffer.fileStart = 0;
+                
+//                 // Append the buffer for processing
+//                 mp4boxfile.appendBuffer(arrayBuffer);
+            
+//                 // Flush to force processing of the appended data
+//                 mp4boxfile.flush();
+            
+//                 // Once the file is ready, you can access its information
+//                 mp4boxfile.onReady = function (info: any) {
+//                     console.log("File info:", info);
+//                 };
+//             }
+//         };
+        
+//         reader.readAsArrayBuffer(file);
+//     }
+// }
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.s?css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ]
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    // tell webpack that we're building for electron
+    target: 'electron-main',
+    node: {
+        // tell webpack that we actually want a working __dirname value
+        // (ref: https://webpack.js.org/configuration/node/#node-__dirname)
+        __dirname: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css"
+        })
+    ]
+};
